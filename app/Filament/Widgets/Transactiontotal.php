dac7637b9ca3ea4bc8dafa313853656203831db3
unknown <elprron007@gmail.com>
2024-06-01 09:48:02 -0500

    widget 1

--- app/Filament/Widgets/Transactiontotal.php
+++ app/Filament/Widgets/Transactiontotal.php
@@ -9,22 +9,31 @@ use App\Models\Transaction;
 
 class TransactionTotal extends BaseWidget
 {
-    //protected function getStats(): array
-   // {
+    protected function getStats(): array
+    {
         // Calcular el total de ingresos
-       // $totalIngresos = Transaction::where('tran_types_id', '=', 1)->sum('amount_tran');
+        $totalIngresos = Transaction::where('type_tran', 'ingreso')->sum('amount_tran');
 
         // Calcular el total de egresos
-       // $totalEgresos = Transaction::where('tran_types_id', '=', 2)->sum('amount_tran');
+        $totalEgresos = Transaction::where('type_tran', 'egreso')->sum('amount_tran');
 
         // Calcular el saldo
-       // $saldo = $totalIngresos - $totalEgresos;
+        $saldoInicial = Transaction::where('type_tran', 'inicial')->sum('amount_tran');
+        $saldo = $saldoInicial + $totalIngresos - $totalEgresos;
 
-       // return [
-           // Stat::make('Ingresos', $totalIngresos),
-           // Stat::make('Egresos', $totalEgresos),
-           // Stat::make('Saldo', $saldo),
-      //  ];
-    //}
-    
+        return [
+            Stat::make('Ingresos', $totalIngresos)
+                ->description('Total de ingresos')
+               // ->descriptionIcon('heroicon-o-trending-up')
+                ->color('success'),
+            Stat::make('Egresos', $totalEgresos)
+                ->description('Total de egresos')
+                //->descriptionIcon('heroicon-o-trending-down')
+                ->color('danger'),
+            Stat::make('Saldo', $saldo)
+                ->description('Saldo total')
+               // ->descriptionIcon('heroicon-o-cash')
+                ->color('primary'),
+        ];
+    }
 }
